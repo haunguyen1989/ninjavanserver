02b3da3c1a49ae694bd3d68aedcec5adfc9e5f65
hau.nguyen <hau.nguyen@isobar.com>
2020-12-30 13:18:52 +0700

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Koa = require('koa');
 const next = require('next');
 
 dotenv.config();
-const port = parseInt(process.env.PORT, 10) || 80;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 
